ed81b20b830be4ff7cee0c15f48d76153582a38d
MELDemy <mahmoudeldemerdash5@gmail.com>
2024-10-07 15:09:55 +0300

    add location feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather/core/services/location/get_location.dart';
 
 void main() {
   runApp(const WeatherApp());
@@ -10,7 +11,37 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: TestLocation(),
+      ),
+    );
+  }
+}
+
+class TestLocation extends StatefulWidget {
+  const TestLocation({super.key});
+
+  @override
+  State<TestLocation> createState() => _TestLocationState();
+}
+
+class _TestLocationState extends State<TestLocation> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCity();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          getCity();
+        },
+        child: Text("Press"),
+      ),
     );
   }
 }
