17a5a7d73715ffee306d36ffccce5876ffd2f75b
Nada <nadawahba250@gmail.com>
2024-10-19 23:55:12 +0300

    Fixed text overflow issue on City Screen and removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Weather App',
       home: ChangeNotifierProvider(
         create: (context) => CityProvider(),
