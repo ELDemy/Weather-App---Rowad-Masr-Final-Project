4d5cbf82d48ef079dc19c9f8c88e91d866b90ea9
mhadd <mhadad315@gmail.com>
2024-10-23 20:16:59 +0300

    notification service

--- lib/core/services/notification/work_manager_services.dart
+++ lib/core/services/notification/work_manager_services.dart
@@ -9,52 +9,10 @@ class WorkManagerServices {
         isInDebugMode:
             true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
         );
-    //startAction();
   }
-
-  // startAction() async {
-  //   Workmanager().registerPeriodicTask(
-  //     "task1",
-  //     "Task1name",
-  //   );
-  //   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-  //       FlutterLocalNotificationsPlugin();
-  //   await flutterLocalNotificationsPlugin.initialize(
-  //     const InitializationSettings(
-  //       android: AndroidInitializationSettings('@mipmap/ic_launcher'),
-  //     ),
-  //   );
-  //   // Initialize the notifications plugin in the background task
-  //   WeatherModel? weatherModel;
-  //   try {
-  //     Either<Failure, WeatherModel> weatherResult =
-  //         await WeatherService().getWeatherForCity("cairo");
-  //
-  //     weatherResult.fold(
-  //       (failure) {
-  //         print("failure");
-  //       },
-  //       (weather) async {
-  //         weatherModel = weather;
-  //         print("ff = ${weatherModel?.current?.temp}");
-  //
-  //         try {
-  //           await LocalNotificationService.showDailyScheduledNotification(
-  //               weatherModel!.current!.temp!);
-  //         } on Exception catch (e) {
-  //           print(e.toString());
-  //         }
-  //         print("Notification Sent");
-  //       },
-  //     );
-  //   } catch (e) {
-  //     print("exception $e");
-  //   }
-  // }
 }
 
-@pragma(
-    'vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+@pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask(
     (taskName, inputData) async {
