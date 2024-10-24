2c4352b7c87e196ff994ca421cf38e28fbdc2791
MELDemy <mahmoudeldemerdash5@gmail.com>
2024-10-24 14:44:17 +0300

    edit home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weather/core/services/notification/notification_service.dart';
-import 'package:weather/views/splash_view.dart';
+import 'package:weather/views/home_screen/CitySelectionPage.dart';
 import 'package:workmanager/workmanager.dart';
 
 import 'core/services/notification/work_manager_services.dart';
@@ -9,6 +9,11 @@ import 'views/home_screen/providers/CityProvider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  runApp(const WeatherApp());
+  initNotification();
+}
+
+Future<void> initNotification() async {
   await LocalNotificationService.init();
   await WorkManagerServices().init();
   Workmanager().registerPeriodicTask(
@@ -17,7 +22,6 @@ void main() async {
     frequency: const Duration(hours: 6),
     initialDelay: const Duration(seconds: 10), // Adjust for testing
   );
-  runApp(const WeatherApp());
 }
 
 class WeatherApp extends StatelessWidget {
